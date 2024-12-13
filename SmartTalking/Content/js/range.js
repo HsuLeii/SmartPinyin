f922485f9c003bcbfb09bdbd57c6c6a136c93639
HsuLeii <ok841113@gmail.com>
2024-12-13 15:08:21 +0800

    1213

--- SmartTalking/Content/js/range.js
+++ SmartTalking/Content/js/range.js
@@ -6,35 +6,35 @@ var playRangeWidth = (value) => {
 }
 
 //取得聲音range
-var soundRange = document.getElementById("soundRange");
+// var soundRange = document.getElementById("soundRange");
 //取得聲音距離
-var soundRangeWidth = (value) => {
-    document.documentElement.style.setProperty("--soundRange", 100 - value + "%");
-    if (value < 1) {
-        $(".icon_volume").removeClass("fa-volume-high");
-        $(".icon_volume").removeClass("fa-volume-low");
-        $(".icon_volume").addClass("fa-volume-xmark");
+// var soundRangeWidth = (value) => {
+//     document.documentElement.style.setProperty("--soundRange", 100 - value + "%");
+//     if (value < 1) {
+//         $(".icon_volume").removeClass("fa-volume-high");
+//         $(".icon_volume").removeClass("fa-volume-low");
+//         $(".icon_volume").addClass("fa-volume-xmark");
 
-    } else if (value < 45) {
-        $(".icon_volume").removeClass("fa-volume-xmark");
-        $(".icon_volume").removeClass("fa-volume-high");
-        $(".icon_volume").addClass(" fa-volume-low");
+//     } else if (value < 45) {
+//         $(".icon_volume").removeClass("fa-volume-xmark");
+//         $(".icon_volume").removeClass("fa-volume-high");
+//         $(".icon_volume").addClass(" fa-volume-low");
 
-    } else {
-        $(".icon_volume").removeClass("fa-volume-low");
-        $(".icon_volume").removeClass("fa-volume-xmark");
-        $(".icon_volume").addClass("fa-volume-high");
-    }
-}
+//     } else {
+//         $(".icon_volume").removeClass("fa-volume-low");
+//         $(".icon_volume").removeClass("fa-volume-xmark");
+//         $(".icon_volume").addClass("fa-volume-high");
+//     }
+// }
 
 //更改播放距離顏色
 function updatePlayRangeProperty() {
     playRangeWidth(playRange.value);
 }
 
-function updateSoundRangeProperty() {
-    soundRangeWidth(soundRange.value);
-}
+// function updateSoundRangeProperty() {
+//     soundRangeWidth(soundRange.value);
+// }
 
 playRange.addEventListener("input", updatePlayRangeProperty);
-soundRange.addEventListener("input", updateSoundRangeProperty);
+// soundRange.addEventListener("input", updateSoundRangeProperty);
